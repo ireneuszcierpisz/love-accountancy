9ebfa9e122bee81710be199e422128fa8beb791c
ireneuszcierpisz <>
2023-10-03 10:59:23 +0000

    Add docstrings and comments. Remove the commented code.

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,10 +13,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love-accountancy')
 
-# fetches values from all of the cells of the sheet to reduce API calls:
+# fetches values from all of the cells of the Trial Balance (TB) spreadsheet 
+# to reduce API calls:
 tb = SHEET.worksheet("Trial Balance").get_all_values()
+# Statement of Profit and Loss (SOPL) and
+# Statement of Financial Position (SOFP) 
+# worksheets as the global variables
 sopl_worksheet = SHEET.worksheet("SOPL")
 sofp_worksheet = SHEET.worksheet("SOFP")
+
 
 def get_gl_codes():
     """
@@ -46,28 +50,25 @@ def get_gl_codes():
             else:
                 i += 1
 
-    # print('Going to proceed with:')
-    # for k,v in data.items():
-    #     print(f'{k}: {v}')
-
     return data
 
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 2 values.
+    Raises ValueError if strings cannot be found as the GL code,
+    or there are more than one position with the same GL code
+    or if there aren't exactly 2 values provided by the user.
     """
     try:
+        # check if the GL code is unique
         for value in values:
             n = 0
             for row in tb:
                 n += row.count(value)
-            # print(f"! Found {n} GL Code '{value}' !")
             if n != 1:
-                raise ValueError(f'"{value}" is in {n} celles, check GL Codes')
-
+                raise ValueError(f'"{value}" is in {n} cells, check GL Codes')
+        # check if there are two values provided
         if len(values) != 2:
             raise ValueError(f'Two values required, provided {len(values)}')
     except ValueError as e:
@@ -79,12 +80,15 @@ def validate_data(values):
 
 def check_balance(data, type_of_fs):
     """
-    Checking if balance in given column of data is equel zero
+    Checking the sum of a given column of numbers in the Trial Balance 
+    worksheet as related to the relevant financial statements
+    and collecting data for the financial statement.
     """
     print('Checking the balance...')
     financial_periods = ['Current Period', 'Previous Period']
-    col = 2  # financial results for current period
-    # dictionary to collect all data needed to create financial raports as SOFP and SOPL
+    col = 2  # financial results column in TB for the current period
+    # dictionary to collect all data from TB needed to create
+    # financial raports SOFP and SOPL:
     collection = {type_of_fs: {}}
     for i in range(len(financial_periods)):
         print(f'  {financial_periods[i]}')
@@ -92,10 +96,6 @@ def check_balance(data, type_of_fs):
         float_column = [float(num.replace(',', '')) for num in column]
         balance = sum(float_column)
         print(f'    {type_of_fs} Balance is:  {balance:,.2f}')
-        # balance = sum([round(n) for n in float_column])
-        # balance = round(balance)
-        # print(f'    {type_of_fs} Balance on Rounded Numbers is: {balance:,}\n')
-
         # collecting data for financial statement worksheet
         collection[type_of_fs][financial_periods[i]] = {}
         for title in set([row[6] for row in data]):
@@ -106,15 +106,17 @@ def check_balance(data, type_of_fs):
                     collection[type_of_fs][financial_periods[i]][title].append(num)
         for k, v in collection[type_of_fs][financial_periods[i]].items():
             collection[type_of_fs][financial_periods[i]][k] = sum(v)
-        # print(collection)
         col += 2
     print('-----------------\n')
     return collection
 
 
 def get_data_for_fs(type_of_fs, user_data):
-    # gets trial balance as a list of lists of strings
-    # gets data entered by user as a dictionary
+    """
+    Uses trial balance as a list of lists,
+    gets as a dictionary data entered by the user
+    and return part of tb as a data for one of financial statements
+    """
 
     print(f'Extracting data from TB for {type_of_fs}...')
     first_row, last_row = 0, 0
@@ -124,12 +126,20 @@ def get_data_for_fs(type_of_fs, user_data):
         if row[0] == user_data[type_of_fs][1]:
             last_row = tb.index(row) + 1
     table_for_fs = tb[first_row - 1:last_row]
-    # return TB worksheet as a list of lists
-    # return name of type of financial statement
+    # return part of the TB worksheet as a list of lists
+    # returns the name of the financial statement type
     return type_of_fs, table_for_fs
 
 
 def make_raport(raport_name, data_collection, g_worksheet):
+    """
+    Prepares financial statement (fs) of a given type.
+    Retrieves the data set for the fs and
+    updates the worksheet with rounded values and
+    changes the sign of the numbers where required.
+    Throws an error if any item in fs is not unique
+    or there is no item that exists in TB.
+    """
     print(f'Generating report {raport_name}...')
     data_list = g_worksheet.get_all_values()
     # gets range of worksheet rows containing assets
@@ -141,7 +151,6 @@ def make_raport(raport_name, data_collection, g_worksheet):
             last = i
     col = 4
     for p in ['Current Period', 'Previous Period']:
-        # print(f'  {p}:')
         for k, v in data_collection[raport_name][p].items():
             row_num = 1
             found = False
@@ -157,7 +166,6 @@ def make_raport(raport_name, data_collection, g_worksheet):
                         g_worksheet.update_cell(row_num, col, round(v))
                     else:
                         g_worksheet.update_cell(row_num, col, round(-1 * v))
-                    # print(f'{k} = {v} , {raport_name} row:{row_num} col:{col}')
                     found = True
                 row_num += 1
             try:
@@ -170,7 +178,13 @@ def make_raport(raport_name, data_collection, g_worksheet):
 
 
 def handle_data(g_worksheet):
-    # ads style to the worksheet heading
+    """
+    Adds a style to the sheet header.
+    Calculates totals in columns for given groups of data,
+    calculates the required sum of totals in the statement and
+    updates the appropriate spreadsheet cells.
+    """
+    # adds style to the worksheet heading
     g_worksheet.format("A1:F2", {
         "backgroundColor": {
             "red": 0.5,
@@ -187,15 +201,17 @@ def handle_data(g_worksheet):
         "fontSize": 11,
         }
     })
-    # get financial statement as a list
     print('   Computing totals...')
+    # gets financial statement as a list
     fs = g_worksheet.get_all_values()
     for col in [3, 5]:
-        t_o_t = 0
+        t_o_t = 0   # total of totals variable
         for i in range(len(fs)):
             total = 0
             if fs[i][0][:5] == "Total":
                 j = 1
+                # the flag indicates whether the cell needs to be updated 
+                # with the t_o_t value
                 flag = False
                 while True:
                     if fs[i - j][col] == '':
@@ -203,7 +219,6 @@ def handle_data(g_worksheet):
                             flag = True
                         break
                     total += float(fs[i - j][col].replace(',', '.'))
-                    # print(f'got r{i-j+1} c{col+1}')
                     j += 1
                 t_o_t += total
                 if flag:
@@ -211,42 +226,41 @@ def handle_data(g_worksheet):
                     t_o_t = 0
                 else:
                     g_worksheet.update_cell(i + 1, col + 1, total)
-                    # print(f'r{i+1} c{col+1} updated')
 
     if g_worksheet == sopl_worksheet:
         compute_loss(fs, g_worksheet)
 
 
 def compute_loss(fs, g_worksheet):
-    # declare operating loss, total finance loss, tax for the financial period variables
-    # ol_current, tfc_current, tftfp_current = 0, 0, 0
-    # ol_previous, tfc_previous, tftfp_previous = 0, 0, 0
-    # gets relevant data from finance statement
+    """
+    Computes Loss Before Tax and Loss for the Financial Period
+    for fs: Statement of Profit and Loss.
+    Updating the appropriate cells in the SOPL report with loss values.
+    """
+    # Retrieves the appropriate data from fs and performs the calculations
     for i in range(len(fs)):
         if "Operating loss" in fs[i][0]:
             ol_current = int(fs[i][3])
-            ol_previous = int(fs[i][5]) 
+            ol_previous = int(fs[i][5])
             continue
         if "Total finance costs" in fs[i][0]:
             tfc_current = int(fs[i][3])
             tfc_previous = int(fs[i][5])
-            # Loss before tax, current period
+            # Loss before tax, current period:
             lbtcp = ol_current + tfc_current
             g_worksheet.update_cell(i + 3, 4, lbtcp)
-            # Loss before tax, previous period
+            # Loss before tax, previous period:
             lbtpp = ol_previous + tfc_previous
             g_worksheet.update_cell(i + 3, 6, lbtpp)
             continue
         if "Tax for the financial period" in fs[i][0]:
             tftfp_current = int(fs[i][3])
             tftfp_previous = int(fs[i][5])
-            # Loss for the financial period, current period
+            # Loss for the financial period, current period:
             lftfpc = lbtcp + tftfp_current
-            # print(f"Loss for the financial period, current period: {lftfpc:,}")
             g_worksheet.update_cell(i + 3, 4, lftfpc)
-            # Loss for the financial period, previous period
+            # Loss for the financial period, previous period:
             lftfpp = lbtpp + tftfp_previous
-            # print(f"Loss for the financial period, previous period: {lftfpp:,}")
             g_worksheet.update_cell(i + 3, 6, lftfpp)
 
 
